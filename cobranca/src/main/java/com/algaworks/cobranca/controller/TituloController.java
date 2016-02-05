94a2e9535a5e1d66771bd23ce4583ae13bf8e678
Raphael Oliveira <raphaelsoliveira@gmail.com>
2016-02-05 13:29:01 -0200

    Criação de objetos MensagemErro, cobranca, DialogoConfirmacaoExclusao. Correção de bug de exclusão concluído até aula 2.17

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
@@ -20,28 +22,29 @@ import com.algaworks.cobranca.repository.Titulos;
 @RequestMapping("/titulos")
 public class TituloController {
 
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
 	@Autowired
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");		
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);		
 		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if (errors.hasErrors()) {
-			return mv;
+			return CADASTRO_VIEW;
 		}
 		
 		
 		titulos.save(titulo);
 		
-		mv.addObject("mensagem", "Título salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
@@ -50,6 +53,24 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
 		return mv;
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo /*Long codigo*/) {
+		//System.out.println(">>>>>>>> codigo recebido: " + codigo);
+		//Titulo titulo = titulos.findOne(codigo);
+		
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+	
+	@RequestMapping(value="{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+		titulos.delete(codigo);
+		
+		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+		return "redirect:/titulos";
 	}
 	
 	@ModelAttribute("todosStatusTitulo")
